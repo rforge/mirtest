aae573137897e24f9dd83f49511a51728fc6083e
sartmann <sartmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-26 10:04:54 +0000

    Added functionality to buildpackage. Removed years in publications of KJ and SA.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mirtest@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- compileable/buildPackage.sh
+++ compileable/buildPackage.sh
@@ -1,5 +1,5 @@
-rm -r miRtest
-rm -r miRtest.Rcheck
+rm -rf miRtest
+rm -rf miRtest.Rcheck
 rm  miRtest_1.0.tar.gz
 rm miRtest.tar.gz
 cp miRtest.R miRtest.bckp
@@ -43,12 +43,13 @@ mv miRtest_1.0.tar.gz miRtest.tar.gz
 
 # Build for windows
 R CMD build --binary miRtest
-rm -r miRtest
+rm -rf miRtest
 tar -xzf miRtest_1.0_R_x86_64-pc-linux-gnu.tar.gz 
 zip -r miRtest.zip miRtest
 mv miRtest_1.0_R_x86_64-pc-linux-gnu.tar.gz miRtest.binary.tar.gz
-rm -r miRtest
-rm -r miRtest.Rcheck
+rm -rf miRtest
+rm -rf miRtest.Rcheck
 rm *.tar
 tar xzf miRtest.tar.gz
+svn add *
 
