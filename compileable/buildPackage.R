e1bf2a4d5f825815a96a3843b50e18f7683c44c2
sartmann <sartmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-31 15:52:28 +0000

    Added automatic "roxygen" installation if it is not yet present.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mirtest@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- compileable/buildPackage.R
+++ compileable/buildPackage.R
@@ -1,3 +1,4 @@
+if(!("roxygen" %in% installed.packages()[,1])) install.packages("roxygen",repos="http://cran.r-project.org")
 library(roxygen)
 package.skeleton("miRtest",code_files="miRtest.R",force=TRUE)
 
