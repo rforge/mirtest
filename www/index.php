42aca37903ed3428fbeb6ccaf8a31f25671e86a6
sartmann <sartmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-29 19:36:43 +0000

    Updated project homepage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mirtest@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,8 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> For further information, please take a look at the <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>miRtest summary page</strong></a>. </p>
 
 </body>
 </html>
